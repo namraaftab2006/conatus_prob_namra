fb723a857947fcee095263260430fa8b5f9f3a16
namraaftab2006 <namraaftab2006@gmail.com>
2025-10-21 18:07:22 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'signup_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(const Factify());
+}
+
+
+class Factify extends StatelessWidget {
+  const Factify({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Factify',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: const SignupPage(),
+
+    );
+  }
+}
